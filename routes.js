4dcdbfb6fc451d5d86d8dcb90cda2a9054d7d987
Oscar Rene Duran Miranda <oscarrene380@gmail.com>
2021-10-08 23:03:02 -0600

    call back message edit

--- routes.js
+++ routes.js
@@ -12,7 +12,7 @@ router.post('/send', (req, res) => {
     client.messages.create({
         body: message,  
         messagingServiceSid: 'MG0906845bc26304d670dc941f22384c92',
-        statusCallback: 'https://messagingutec.herokuapp.com/send',
+        statusCallback: 'https://messagingutec.herokuapp.com',
         to: phone 
     })
     .then((message) => {
